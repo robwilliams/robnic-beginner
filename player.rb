b5dedee59dd211b24d913b1b3b7bf83d75e0e77f
Robert Williams <rob@r-williams.com>
2014-08-23 03:04:29 +0100

    Passed Level 6

--- player.rb
+++ player.rb
@@ -1,58 +1,137 @@
-class Player
-  def play_turn(warrior)
+require 'delegate'
 
-    if unhealthy?(warrior) && !safe_to_rest?(warrior)
-      warrior.walk!(:backward)
-      @previous_health = warrior.health
-      return
+class Player
+
+  def actions
+    [
+      WarriorRescue,
+      WarriorRunToStairs,
+      WarriorRecoupHealth,
+      WarriorRetreat,
+      WarriorAttack,
+      WarriorWalk,
+    ]
+  end
+
+  def play_turn(warrior)
+    warrior = AwesomeWarrior.new(warrior, session)
+
+    actions.each do |action| 
+      action = action.new(warrior, session) 
+      return action.play_turn if action.run?
+    end
+  end
+
+  private
+  class WarriorSession
+    def previous_health
+      @previous_health ||= 0
     end
 
-    if !rescued?
-      handle_captive_behind(warrior)
-    elsif warrior.feel.enemy?
-      warrior.attack!
-    elsif warrior.feel.captive?
-      warrior.rescue!
-    else
-      if unhealthy_and_safe_to_rest?(warrior)
-        warrior.rest!
-      else
-        warrior.walk!
+    def rescued!
+      @rescued = true
+    end
+
+    def rescued?
+      @rescued
+    end
+
+    def end_turn(warrior)
+      self.previous_health = warrior.health
+    end
+
+    private
+    def previous_health=(health)
+      @previous_health = health
+    end
+  end
+
+  class WarriorAction < Struct.new(:warrior, :session)
+    def play_turn
+      puts self.class.name
+      perform_actions!
+      session.end_turn(warrior)
+    end
+
+    private
+    def end_turn
+      session.previous_health = warrior.health
+    end
+  end
+
+  class WarriorRescue < WarriorAction
+    def perform_actions!
+      if warrior.feel(:backward).empty?
+        warrior.walk!(:backward)
+      elsif warrior.feel(:backward).captive? 
+        warrior.rescue!(:backward)
+        session.rescued!
       end
     end
 
-    @previous_health = warrior.health
-  end
-
-  def unhealthy_and_safe_to_rest?(warrior)
-    unhealthy?(warrior) && safe_to_rest?(warrior)
-  end
-
-  def unhealthy?(warrior)
-    warrior.health < 6
-  end
-
-  def safe_to_rest?(warrior)
-    @previous_health <= warrior.health
-  end
-
-  def handle_captive_behind(warrior)
-    if warrior.feel(:backward).empty?
-      # as long as we haven't already hit the wall
-      warrior.walk!(:backward)
-    elsif warrior.feel(:backward).captive? 
-      warrior.rescue!(:backward)
-      rescued!
-    elsif warrior.feel(:backward).wall?
-      warrior.walk!(:forward)
+    def run?
+      !session.rescued?
     end
   end
 
-  def rescued?
-    @rescued ||= false
+  class WarriorRunToStairs < WarriorAction
+    def perform_actions!
+      warrior.walk!
+    end
+
+    def run?
+      warrior.feel.stairs? && !warrior.feel.enemy?
+    end
   end
 
-  def rescued!
-    @rescued = true
+  class WarriorRecoupHealth < WarriorAction
+    def perform_actions!
+      warrior.rest!
+    end
+
+    def run?
+      warrior.health >= session.previous_health && warrior.health < 20
+    end
+  end
+
+  class WarriorRetreat < WarriorAction
+    def perform_actions!
+      warrior.walk!(:backward)
+    end
+
+    def run?
+      warrior.health < session.previous_health && warrior.health <= 10
+    end
+  end
+
+  class WarriorAttack < WarriorAction
+    def perform_actions!
+      warrior.attack!
+    end
+
+    def run?
+      warrior.feel.enemy?
+    end
+  end
+
+  class WarriorWalk < WarriorAction
+    def perform_actions!
+      warrior.walk!
+    end
+
+    def run?
+      true
+    end
+  end
+
+  def session
+    @session ||= WarriorSession.new
+  end
+
+  class AwesomeWarrior < SimpleDelegator
+    def initialize(warrior, session)
+      @session = session
+      super(warrior)
+    end
   end
 end
